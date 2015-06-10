eb61a2177f88741296dc3860306befe779e12829
plutoshe <plutoshe@gmail.com>
2015-06-10 16:53:44 +0800

    mapreduce: test process sentence
    a test for process a sentence whose length is between 3 and 30

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import server "github.com/plutoshe/mr/sample_user_server"
+import server "github.com/plutoshe/mr/sample_user_server/processSentence"
 
 func main() {
 	server.Main()
---
+++ sample_user_server/processSentence/processSentence_server.go
@@ -0,0 +1,87 @@
+package server
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/plutoshe/mr/proto"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 10000, "The server port")
+	tp   = flag.String("type", "", "The server type, m(mapper)/r(reducer)")
+	s    *grpc.Server
+)
+
+type server struct{}
+
+func (*server) GetCollectResult(KvPair *pb.ReducerRequest, stream pb.Reducer_GetCollectResultServer) error {
+	fmt.Println("===in Collect Function")
+	if KvPair.Key == "Stop" && len(KvPair.Value) == 0 {
+		// server.Stop()
+		s.Stop()
+		fmt.Println("Stop")
+		return nil
+	}
+	stream.Send(&pb.ReducerResponse{Key: KvPair.Key, Value: ""})
+
+	return nil
+}
+
+func (*server) GetEmitResult(KvPair *pb.MapperRequest, stream pb.Mapper_GetEmitResultServer) error {
+	fmt.Println("===in Emit Function")
+
+	if KvPair.Value == "Stop" && KvPair.Key == "Stop" {
+		// server.Stop()
+		s.Stop()
+		fmt.Println("Stop")
+		return nil
+	}
+
+	chop := ""
+	for i := range KvPair.Key {
+		cc := KvPair.Key[i]
+		if cc == '，' || cc == '。' || cc == '？' || cc == '！' || cc == '；' {
+			if len(chop) >= 3 && len(chop) <= 30 {
+				res := &pb.MapperResponse{
+					Key:   chop,
+					Value: "1",
+				}
+				// fmt.Println(chop[i])
+				if err := stream.Send(res); err != nil {
+					return err
+				}
+				chop = ""
+			}
+		} else {
+			chop += cc
+		}
+	}
+	return nil
+}
+
+func Main() {
+	flag.Parse()
+	if *tp == "" {
+		log.Fatalln("Need a server type m/r")
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	fmt.Println("Listening...")
+
+	if *tp == "m" {
+		s = grpc.NewServer()
+		pb.RegisterMapperServer(s, &server{})
+		s.Serve(lis)
+	} else {
+		s = grpc.NewServer()
+		pb.RegisterReducerServer(s, &server{})
+		s.Serve(lis)
+	}
+}
